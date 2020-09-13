4c4942a0e0edf43351fd7e0976095be77750ed4c
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-09-13 15:04:36 +0530

    add function to get all details in uploaded file

--- SignDataBaseConnection/SignUpDataBase.php
+++ SignDataBaseConnection/SignUpDataBase.php
@@ -5,6 +5,10 @@
 
         $file = $_FILES['ProfilePicture'];
         $fileName = $_FILES['ProfilePicture']['name'];
+        $fileTmpName = $_FILES['ProfilePicture']['tmp_name'];
+        $fileSize = $_FILES['ProfilePicture']['size'];
+        $fileError = $_FILES['ProfilePicture']['error'];
+        $fileType = $_FILES['ProfilePicture']['type'];
 
     }
 
