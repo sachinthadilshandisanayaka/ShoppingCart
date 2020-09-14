b7e520853fa98dbc5c6458f1916cdf14206e453c
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-09-15 00:04:54 +0530

    change logging.php file

--- Logging.php
+++ Logging.php
@@ -9,13 +9,12 @@
 <form action="SignUp.php" class="navigate-logging">
         <button type="submit">Sign up</button>
     </form>
-    <form action="LogInDataBase.php" method="GET">
+    <form action="SignDataBaseConnection/LogInDataBase.inc.php" method="GET">
         <h2>Logging</h2>
-        <label for="username">User Name</label> <input type="text" name="username" placeholder="User Name"> <br><br>
         <label for="email">Email</label> <input type="email" name="email" placeholder="User Name"> <br><br>
         <label for="password">Password</label> <input type="password" name="password" placeholder="User Name"> <br><br>
         <input type="submit" value="submit">
     </form>
-    
+
 </body>
 </html>
