db516f20eab10448c284298beb294748814ae7e1
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-09-19 17:31:05 +0530

    check email is already used

--- AdminPanel/adminDataBase/addSellerDataBase.inc.php
+++ AdminPanel/adminDataBase/addSellerDataBase.inc.php
@@ -2,5 +2,17 @@
     require "../../SignDataBaseConnection/db.inc.php";
     echo "works";
 
+    $SEmail = $_REQUEST['uemail'];
+    
     $sql = "SELECT * FROM sellerDetails WHERE SEmail=:semail";
+    $stm = $conn->prepare($sql);
+    $stm->bindParam(':semail',$SEmail);
+    $stm->execute();
+
+    if ( $stm->rowCount() > 0) {
+        echo "Email is already used";
+    } else {
+        echo "OK...";
+    }
+
 ?>
