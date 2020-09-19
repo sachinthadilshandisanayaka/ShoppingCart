1348c3b2c826f2722d93b25ad9f18f33d39dc124
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-09-19 17:37:36 +0530

    add veriable for upload image

--- AdminPanel/adminDataBase/addSellerDataBase.inc.php
+++ AdminPanel/adminDataBase/addSellerDataBase.inc.php
@@ -13,6 +13,18 @@
         echo "Email is already used";
     } else {
         echo "OK...";
+        $file = $_FILES['file'];
+        $fileName = $_FILES['file']['name'];
+        $fileTmpName = $_FILES['file']['tmp_name'];
+        $fileSize = $_FILES['file']['size'];
+        $fileError = $_FILES['file']['error'];
+        $fileType = $_FILES['file']['type'];
+        $fileExt = explode('.', $fileName);
+        $fileActualExt = strtolower(end($fileExt));
+        $allow = array('jpg', 'jpeg', 'png');
+
+
+
     }
 
 ?>
