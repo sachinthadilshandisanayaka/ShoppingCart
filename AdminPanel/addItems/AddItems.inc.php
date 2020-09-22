f605a6c3177c52b568177c3bcf5a3ec62deabdcf
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-09-22 23:16:31 +0530

    fixed error

--- AdminPanel/addItems/AddItems.inc.php
+++ AdminPanel/addItems/AddItems.inc.php
@@ -1,5 +1,36 @@
 <?php
     require "../../SignDataBaseConnection/db.inc.php";
-
-
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="sellers">
+        <?php
+        try{
+            $stm = $conn->prepare("SELECT * FROM sellerdetails");
+            $stm->execute();
+            $result = $stm->fetchAll();
+            if ( !$result){
+                echo "can not";
+            }
+            if ($result == null) {
+                echo "<p>No Sellers</p>";
+            } else{
+                foreach($result as $row){
+                    echo $row['SName']." ".$row['SAddress']."<br>";
+                }
+            }
+        } catch(PDOException $e) {
+            echo $e;
+        }
+          
+        ?>
+    </div>
+
+</body>
+</html>
