d744e2447d6a3ff5cbc81514055eed9b0bb1b211
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-09-23 17:39:59 +0530

    get database connection

--- AdminPanel/addItems/additemDataBase/AdditemDB.inc.php
+++ AdminPanel/addItems/additemDataBase/AdditemDB.inc.php
@@ -1,3 +1,11 @@
 <?php
     require '../../../SignDataBaseConnection/db.inc.php';
+    
+    try {
+        $stm = $conn->prepare("INSERT INTO selleritems (IDescription,IPrice,IQuntity,SupID,IName) VALUES 
+        (:ides,:iprice,:iqun,:isid,:iname)");
+
+    } catch(PDOException $e){
+        echo $e;
+    }
 ?>
