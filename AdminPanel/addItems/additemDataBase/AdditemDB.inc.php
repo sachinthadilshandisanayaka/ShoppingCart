85ed4e794fa87e15f9a2b7a31b25d90ed4364714
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-09-25 00:23:00 +0530

    set variables to get image file details

--- AdminPanel/addItems/additemDataBase/AdditemDB.inc.php
+++ AdminPanel/addItems/additemDataBase/AdditemDB.inc.php
@@ -18,6 +18,39 @@
         $stm->execute();
 
         if($stm->rowCount() == 1){
+            $file = $_FILES['pfile'];
+            $fileName = $_FILES['pfile']['name'];
+            $fileTmpName = $_FILES['pfile']['tmp_name'];
+            $fileSize = $_FILES['pfile']['size'];
+            $fileError = $_FILES['pfile']['error'];
+            $fileType = $_FILES['pfile']['type'];
+            $fileExt = explode('.', $fileName);
+            $fileActualExt = strtolower(end($fileExt));
+            $allow = array('jpg', 'jpeg', 'png');
+
+            // second image
+            $file2 = $_FILES['pfile2'];
+            $fileName2 = $_FILES['pfile2']['name'];
+            $fileTmpName2 = $_FILES['pfile2']['tmp_name'];
+            $fileSize2 = $_FILES['pfile2']['size'];
+            $fileError2 = $_FILES['pfile2']['error'];
+            $fileType2 = $_FILES['pfile2']['type'];
+            $fileExt2 = explode('.', $fileName2);
+            $fileActualExt2 = strtolower(end($fileExt2));
+
+            // third image
+            $file3 = $_FILES['pfile3'];
+            $fileName3 = $_FILES['pfile3']['name'];
+            $fileTmpName3 = $_FILES['pfile3']['tmp_name'];
+            $fileSize3 = $_FILES['pfile3']['size'];
+            $fileError3 = $_FILES['pfile3']['error'];
+            $fileType3 = $_FILES['pfile3']['type'];
+            $fileExt3 = explode('.', $fileName3);
+            $fileActualExt3 = strtolower(end($fileExt3));
+            
+            
+
+
             // header("Location: ../AddItems.inc.php?er=1");
         }
 
