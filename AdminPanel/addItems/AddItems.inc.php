af2188185c097f64f804e77c642cf0b86047c0f0
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-09-24 23:50:21 +0530

    add css

--- AdminPanel/addItems/AddItems.inc.php
+++ AdminPanel/addItems/AddItems.inc.php
@@ -89,16 +89,23 @@
                     <input type="text" placeholder="Quantity" name="itemQuantity" required>
 
                     <label for="ProductPhoto"><b>Item Photo</b></label>
-                    <input type="file" name="pfile" class="inputItems" id="inputItems0" required>
-                    <input type="radio" name="selectImage" id="selectImage1" value="1">
+                    <div class="image">
+                        <input type="file" name="pfile" class="inputItems" id="inputItems0" required>
+                        <input type="radio" name="selectImage" id="selectImage1" value="1">
+                    </div>
 
                     <label for="ProductPhoto2"><b>Item Photo</b></label>
-                    <input type="file" name="pfile2" class="inputItems1" id="inputItems1" required>
-                    <input type="radio" name="selectImage" id="selectImage2" value="2">
+                    <div class="image">
+                        <input type="file" name="pfile2" class="inputItems1" id="inputItems1" required>
+                        <input type="radio" name="selectImage" id="selectImage2" value="2">    
+                    </div>
+                    
 
                     <label for="ProductPhoto3"><b>Item Photo</b></label>
-                    <input type="file" name="pfile3" class="inputItems2" id="inputItems2" required>
-                    <input type="radio" name="selectImage" id="selectImage3" value="3">
+                    <div class="image">
+                        <input type="file" name="pfile3" class="inputItems2" id="inputItems2" required>
+                        <input type="radio" name="selectImage" id="selectImage3" value="3">    
+                    </div>
 
                 <div class="clearfix">
                     <button type="button" onclick="closeForm()" class="cancelbtn">Cancel</button>
