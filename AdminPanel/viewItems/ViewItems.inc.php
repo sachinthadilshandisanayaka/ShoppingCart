98ab7dee24e250d50284314932cf04c5f861ca96
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-09-27 13:51:22 +0530

    updated

--- AdminPanel/viewItems/ViewItems.inc.php
+++ AdminPanel/viewItems/ViewItems.inc.php
@@ -1,3 +1,6 @@
+<?php
+    require "../../SignDataBaseConnection/db.inc.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,13 +18,37 @@
 
     </h1>
     <section class="right-nav">
+        <?php
+            $stm = $conn->prepare("SELECT * FROM itemphoto WHERE display = 1");
+            $stm->execute();
+            $result = $stm->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stm->fetchAll();
 
+            if(sizeof($result) != 0){   
+                if(sizeof($result)%4 == 0){
+                    echo "<div class=\"display-item\" style=\"grid-template-columns: 25% 25% 25% 25%;\">";
+                } elseif(sizeof($result)%3 == 0){
+                    echo "<div class=\"display-item\" style=\"grid-template-columns: 33.33% 33.33% 33.33%;\">";                        
+                } elseif(sizeof($result)%2 == 0){
+                    echo "<div class=\"display-item\" style=\"grid-template-columns: 50% 50%;\">";
+                } else{
+                    echo "<div class=\"display-item\" style=\"grid-template-columns: 50% 50%;\">";
+                }
+                echo "";
+           
+            foreach($result as $row){
+                echo "<div class=\"item\">";
+                echo "<img src='../adminDataBase/itemUploads/".$row['photo']."'>";
+                echo "</div>";
+            }
+            echo "</div>";
+        }
+        ?>
     </section>
 
     <section class="left-nav">
-        <div class="modify" onclick="document.getElementById('id01').style.display='block'">Add seller</div>
-        <div class="modify" onclick="location.href='addItems/AddItems.inc.php'">Add Item</div>
-        <div class="modify">View Items</div>
+        <div class="modify" onclick="location.href='../adminPanel.inc.php'">Main menu</div>
+        <div class="modify" onclick="location.href='../addItems/AddItems.inc.php'">Add Item</div>
         <div class="modify">Remove seller</div>
         <div class="modify">Orders</div>
         <div class="modify">Add orders</div>
