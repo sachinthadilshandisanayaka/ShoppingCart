e54ecbbcc1d949cd68851c0599ec1fe9eb46a65b
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-10-01 23:22:27 +0530

    updated

--- AdminPanel/viewSeller/viewSeller.inc.php
+++ AdminPanel/viewSeller/viewSeller.inc.php
@@ -1,3 +1,14 @@
 <?php
     require "../../SignDataBaseConnection/db.inc.php";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>view seller</title>
+</head>
+<body>
+    
+</body>
+</html>
