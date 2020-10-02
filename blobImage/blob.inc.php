15811ec8b86ae3740b16472bc14ce198cb94e2d8
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-10-02 09:58:34 +0530

    add image blob

---
+++ blobImage/blob.inc.php
@@ -0,0 +1,19 @@
+<form action="blob.inc.php" method="POST" enctype="multipart/form-data">
+    <input type="file" name="file">
+    <button type="submit" name="submit">ok</button>
+</form>
+
+<?php
+    require '../SignDataBaseConnection/db.inc.php';
+
+    if(isset($_POST['submit'])){
+        $imageData = $_FILES['file']['tmp_name'];
+
+        // echo $imageData;
+        // header("content-type : image/png");
+        echo '<img src="data:image/png;base64,'.base64_encode(file_get_contents($imageData)).'">'; 
+              
+    }
+   
+
+?>
