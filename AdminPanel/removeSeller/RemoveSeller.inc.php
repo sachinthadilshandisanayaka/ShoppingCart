654ffb950057dec933ed1fbad520c427ece7b405
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-10-06 21:06:12 +0530

    add css

--- AdminPanel/removeSeller/RemoveSeller.inc.php
+++ AdminPanel/removeSeller/RemoveSeller.inc.php
@@ -6,9 +6,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/RemoveSeller.css">
     <title>Remove Seller</title>
 </head>
 <body>
     <h1>Remove Seller</h1>
+    <a href="../AdminPanel.inc.php" class="back"><button>Back</button></a>
+    
 </body>
 </html>
