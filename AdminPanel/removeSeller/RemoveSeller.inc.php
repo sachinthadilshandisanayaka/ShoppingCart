8d430245a15cf074e947f213e2442a546fbf6289
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-10-06 20:49:02 +0530

    set methods in remove seller file

---
+++ AdminPanel/removeSeller/RemoveSeller.inc.php
@@ -0,0 +1,14 @@
+<?php
+ include '../../SignDataBaseConnection/db.inc.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Remove Seller</title>
+</head>
+<body>
+    
+</body>
+</html>
