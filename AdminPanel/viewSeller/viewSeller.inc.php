9f8c5df427c60be021098b27e5f2b8cb3201ad3f
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-10-08 22:17:47 +0530

    functions added to the view Seller.inc.php

--- AdminPanel/viewSeller/viewSeller.inc.php
+++ AdminPanel/viewSeller/viewSeller.inc.php
@@ -13,7 +13,35 @@
 <body>
     <?php
         $stm = $conn->prepare("SELECT * FROM sellerdetails");
-        
+        $stm->execute();
+        $result = $stm->fetchAll();
+
+        echo "<section class=\"sellers\">";
+
+        if(sizeof($result) == null) {
+            echo "<div class=\"seller\"> No sellers </div>";
+        } else {
+           
+            foreach($result as $row) {
+                $photoName = $row['sphoto'];
+                $passValue = $row['sname'];
+                if($row['sphoto'] == null){
+                    $photoName = "defaultP.png";
+                }
+
+                echo "<div class=\"seller\">";
+                echo "<div> 
+                    <img src=\"../adminDataBase/uploads/".$photoName."\">
+                </div>";
+                echo "<p>Name : ".$row['sname']."</p>";
+                echo "<p>Name : ".$row['saddress']."</p>";
+                echo "<p>Name : ".$row['semail']."</p>"; 
+                echo "<p>Name : ".$row['sphone']."</p>";
+            } 
+            echo "</div>";
+        }
+
+        echo "</section>";
     ?>
     
 </body>
