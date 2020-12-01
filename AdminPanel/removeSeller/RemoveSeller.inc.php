5eb7da5727f15d465b08f83937b0201ccff0e6a6
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-12-01 23:05:24 +0530

    updated

--- AdminPanel/removeSeller/RemoveSeller.inc.php
+++ AdminPanel/removeSeller/RemoveSeller.inc.php
@@ -3,12 +3,14 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/RemoveSeller.css">
     <title>Remove Seller</title>
 </head>
+
 <body>
     <h1>Remove Seller</h1>
     <a href="../AdminPanel.inc.php" class="back"><button>Back</button></a>
@@ -17,7 +19,27 @@
         <div class="allSellers">
             <?php
                 try{
-                    $stm = $conn->prepare("SELECT * FROM ");
+                    $stm = $conn->prepare("SELECT * FROM sellerdetails");
+                    $stm->execute();
+                    $result = $stm->fetchAll();
+
+                    echo "<section class=\"sellers\">";
+
+                        if(sizeof($result) == 0) {
+                            echo "<div class=\"no_seller\"> No Sellers </div>";
+                        } else {
+                            echo "<div class=\"seller\">";
+                                echo "<div class=\"seller_image\">
+                                        <img src=\"../adminDataBase/uploads/".$photoName."\">
+                                        </div>";
+                                echo "<p>Name : ".$row['sname']."</p>";
+                                echo "<p>address : ".$row['saddress']."</p>";
+                                echo "<p>email : ".$row['semail']."</p>"; 
+                                echo "<p>phone number : ".$row['sphone']."</p>";
+                            echo "</div>"; // seller class
+                        }
+
+                    echo "</section>";
 
                 } catch(PDOException $e) {
                     echo "Error :".$e;
@@ -26,6 +48,7 @@
             ?>
         </div>
     </section>
-    
+
 </body>
+
 </html>
